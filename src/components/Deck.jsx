6227a5fbf9dd6f831d3ef049bea7c24014265dbd
nathbalica <nathbalica@gmail.com>
2023-05-17 13:51:15 -0300

    project organization and components

---
+++ src/components/Deck.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Deck(){
+    return (
+        <ContainerMain>
+
+        </ContainerMain>
+    )
+}
+
+const ContainerMain = styled.div `
+    
+`
